789219453e0f2f1ce36ea72c7bd53c3ed7569761
KommonO <kousley@uccs.edu>
2016-05-03 01:52:39 -0600

    Push before synthesis attempt

--- vga_controller.v
+++ vga_controller.v
@@ -15,26 +15,35 @@
 //INTERMEDIATE:ARROWS ENTER X, O
 //ADVANCED:ARROWS ENTER ANY CHARACTER
 //Will also need tp set several count values for the different timers, refer to the washer machinr timers
+//NET 	"RD"			LOC = "H14"	| IOSTANDARD = LVTTL | DRIVE = 8 | SLEW = FAST;
+//NET 	"GR"			LOC = "H15"	| IOSTANDARD = LVTTL | DRIVE = 8 | SLEW = FAST;
+//NET 	"BL"			LOC = "G15"	| IOSTANDARD = LVTTL | DRIVE = 8 | SLEW = FAST;
+//NET 	"HS"			LOC = "F15"	| IOSTANDARD = LVTTL | DRIVE = 8 | SLEW = FAST;
+//NET 	"VS"			LOC = "F14"	| IOSTANDARD = LVTTL | DRIVE = 8 | SLEW = FAST;
 
-module vga_controller(input mclk, rst, output VGA_RED, VGA_GREEN, VGA_BLUE, VGA_HSYNC, VGA_VSYNC);
+module vga_controller(input BTN0, CCLK , output  reg VGA_RED, VGA_GREEN, VGA_BLUE, VGA_HSYNC, VGA_VSYNC);
 //counters are found in thr vga_timer
+wire clk_50mhz, hsync_out,vsync_out,vga_on;
+wire [10:0] Pixel_X;
+wire [10:0] Pixel_Y;
 
-//Parameter Count values
-parameter PULSE_WIDTH_COUNT_MAX;
+assign hsync = ~hsync_out;
+assign vsync = ~vsync_out;
+
 
 //processes
-always @(posedge mclk or posedge rst) begin
+always @(posedge CCLK or posedge BTN0) begin
   //If reset,colors set to 0 to print out black.correct?
-  if(rst) begin
-    VGA_RED =1'B0;
-    VGA_BLUE =1'B0;
-    VGA_GREEN =1'B0;
+  if(BTN0) begin
+    VGA_RED <=1'b0;
+    VGA_BLUE <=1'b0;
+    VGA_GREEN <=1'b0;
   end
 
-  else if(video_on) begin
-    VGA_RED = TicTacToeR;
-    VGA_BLUE =TicTacToeB;
-    VGA_GREEN = TicTacToeG;
+  else if(vga_on) begin
+    VGA_RED <= 1;//TicTacToeR;
+    VGA_BLUE <=1;//TicTacToeB;
+    VGA_GREEN <= 1;//TicTacToeG;
   end
   //else if video is not on screen should be black
   else begin
@@ -46,9 +55,9 @@ end
 
 
 //initialize other modules
-vga_timer vga_timer();
+vga_timer vga_timer(.mclk(clk_50mz), .clr(BTN0),.hsync(hsync_out), .vsync(vsync_out),.Pixel_X(Pixel_X), .Pixel_Y(Pixel_Y), .vga_on(vga_on));
 
-ttt_logic ttt_logic();
+//ttt_logic ttt_logic();
 //ASCCII TRANSLATE
 
 
